fe4aaaeae89621993780c9707c3141a4fdd6f45e
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2025-12-04 08:18:22 +0000

    Added consistent path.join calls
    Signed-off-by: Jaidlab <240663663+Jaidlab@users.noreply.github.com>

--- x/build.ts
+++ x/build.ts
@@ -7,12 +7,12 @@ import {renderHtmlHandlebars} from "zeug"
 const srcDir = path.resolve(import.meta.dir, '..', 'src')
 const outDir = path.resolve(import.meta.dir, '..', 'out')
 
-const htmlTemplate = await Bun.file(`${srcDir}index.html.hbs`).text()
-const sassResult = await sass.compileAsync(`${srcDir}index.sass`, {
+const htmlTemplate = await Bun.file(path.join(srcDir, 'index.html.hbs')).text()
+const sassResult = await sass.compileAsync(path.join(srcDir, 'index.sass'), {
   style: "compressed",
 })
 
-const imagePath = `${srcDir}bun.webp`
+const imagePath = path.join(srcDir, 'bun.webp')
 const imageFile = Bun.file(imagePath)
 const imageBuffer = await imageFile.arrayBuffer()
 const base64 = Buffer.from(imageBuffer).toString("base64")
@@ -33,6 +33,6 @@ const minifiedHtml = await minify(html, {
   minifyCSS: true,
 })
 
-await Bun.write(`${outDir}index.html`, minifiedHtml)
+await Bun.write(path.join(outDir, 'index.html'), minifiedHtml)
 
-console.log(`Built to ${outDir}index.html`)
+console.log(`Built to ${path.join(outDir, 'index.html')}`)
