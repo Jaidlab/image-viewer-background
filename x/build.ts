b995c208aeb0eacbd74c99dc30bba98ab45e7d1d
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2025-12-04 08:09:25 +0000

    Initial release
    Signed-off-by: Jaidlab <240663663+Jaidlab@users.noreply.github.com>

---
+++ x/build.ts
@@ -0,0 +1,36 @@
+import * as sass from "sass"
+import {minify} from "html-minifier-terser"
+import {renderHtmlHandlebars} from "zeug"
+
+const srcDir = new URL("../src/", import.meta.url).pathname.slice(1)
+const outDir = new URL("../out/", import.meta.url).pathname.slice(1)
+
+const htmlTemplate = await Bun.file(`${srcDir}index.html.hbs`).text()
+const sassResult = await sass.compileAsync(`${srcDir}index.sass`, {
+  style: "compressed",
+})
+
+const imagePath = `${srcDir}bun.webp`
+const imageFile = Bun.file(imagePath)
+const imageBuffer = await imageFile.arrayBuffer()
+const base64 = Buffer.from(imageBuffer).toString("base64")
+const imageUrl = `data:image/webp;base64,${base64}`
+
+const html = renderHtmlHandlebars(htmlTemplate, {
+  CSS: sassResult.css,
+  imageUrl,
+})
+
+const minifiedHtml = await minify(html, {
+  collapseWhitespace: true,
+  removeComments: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  useShortDoctype: true,
+  minifyCSS: true,
+})
+
+await Bun.write(`${outDir}index.html`, minifiedHtml)
+
+console.log(`Built to ${outDir}index.html`)
