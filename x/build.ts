d3f265ce133fe8cf5c97759d95b1eccdca8da54d
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2025-12-06 12:02:09 +0000

    Improved building
    Signed-off-by: Jaidlab <240663663+Jaidlab@users.noreply.github.com>

--- x/build.ts
+++ x/build.ts
@@ -3,28 +3,38 @@ import path from 'path'
 import * as sass from "sass"
 import {minify} from "html-minifier-terser"
 import sharp from 'sharp'
+import fs from 'fs-extra'
+
+import inputHtml from "src/index.html" with { type: "text" }
 
 const srcDir = path.resolve(import.meta.dir, '..', 'src')
 const outDir = path.resolve(import.meta.dir, '..', 'out', 'page')
+fs.emptyDirSync(outDir)
 
-const sassResult = sass.compile(path.join(srcDir, 'index.sass'))
+const buildCss = async () => {
+  const sassResult = sass.compile(path.join(srcDir, 'index.sass'))
+  return Bun.write(path.join(outDir, 'main.css'), sassResult.css)
+}
 
-await Bun.write(path.join(outDir, 'main.css'), sassResult.css)
+const buildImage = async () => {
+  return sharp(path.join(srcDir, 'bun.jxl')).avif({effort:9,quality:90}).toFile(path.join(outDir, 'bun.avif'))
+}
 
-await sharp(path.join(srcDir, 'bun.jxl')).avif({effort:9,quality:90}).toFile(path.join(outDir, 'bun.avif'))
+const buildHtml = async () => {
+  const minifiedHtml = await minify(inputHtml, {
+    collapseWhitespace: true,
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    useShortDoctype: true,
+    minifyCSS: true,
+  })
+  return Bun.write(path.join(outDir, 'index.html'), minifiedHtml)
+}
 
-const htmlTemplate = await Bun.file(path.join(srcDir, 'index.html')).text()
-
-const minifiedHtml = await minify(htmlTemplate, {
-  collapseWhitespace: true,
-  removeComments: true,
-  removeRedundantAttributes: true,
-  removeScriptTypeAttributes: true,
-  removeStyleLinkTypeAttributes: true,
-  useShortDoctype: true,
-  minifyCSS: true,
-})
-
-await Bun.write(path.join(outDir, 'index.html'), minifiedHtml)
-
-console.log(`Built to ${outDir}`)
+await Promise.all([
+  buildCss(),
+  buildImage(),
+  buildHtml(),
+])
