e631164332383475a7db2a1415ceee1a7d0f621a
Jaidlab <240663663+Jaidlab@users.noreply.github.com>
2025-12-06 12:31:20 +0000

    Testing

--- x/build.ts
+++ x/build.ts
@@ -18,7 +18,7 @@ const buildCss = async () => {
 
 const buildImage = async () => {
   const inputFile = path.join(srcDir, 'bun.jxl')
-  const outputFile = path.join(outDir, 'bun.webp')
+  const outputFile = path.join(outDir, 'bun.avif')
   return sharp(inputFile).avif().toFile(outputFile)
 }
 
